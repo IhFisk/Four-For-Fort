5c30774cb5af92e1bb5ff228fd9c3cb0681089ee
FlDeLima <florian.de_lima@hotmail.com>
2019-03-01 18:37:01 -0500

    Revert "Revert "Add multitest scene""
    This reverts commit fbf858cbede54a46a2a5f6f4ab6c66ef379b55ba.

---
+++ WonderJam/Assets/Scripts/CGameManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+public class CGameManager : Photon.MonoBehaviour
+{
+
+    // this is a object name (must be in any Resources folder) of the prefab to spawn as player avatar.
+    // read the documentation for info how to spawn dynamically loaded game objects at runtime (not using Resources folders)
+    public string playerPrefabName = "Personnage";
+    private string roomName = "myRoom2";
+    //public CSpawnManager spawnManager;
+
+    void OnJoinedRoom()
+    {
+
+        StartGame();
+        ScoreExtensions.SetScore(PhotonNetwork.player, 0);
+    }
+
+
+    private void OnConnectedToMaster()
+    {
+        if (PhotonNetwork.GetRoomList().Length == 0)
+
+            PhotonNetwork.CreateRoom(roomName = null);
+        else
+            PhotonNetwork.JoinRandomRoom();
+        //PhotonNetwork.LoadLevel("Scene02");
+        //StartGame();
+    }
+
+    IEnumerator OnLeftRoom()
+    {
+        //Easy way to reset the level: Otherwise we'd manually reset the camera
+
+        //Wait untill Photon is properly disconnected (empty room, and connected back to main server)
+        while (PhotonNetwork.room != null || PhotonNetwork.connected == false)
+            yield return 0;
+
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+
+    void StartGame()
+    {
+        //empeche le groupe 1 d'evoyer 
+        //PhotonNetwork.SetSendingEnabled(1, false);
+
+        //empeche le groupe 1 de recevoir 
+        //PhotonNetwork.SetReceivingEnabled(1, false);
+
+        Camera.main.farClipPlane = 1000; //Main menu set this to 0.4 for a nicer BG    
+
+        //prepare instantiation data for the viking: Randomly diable the axe and/or shield
+        /*bool[] enabledRenderers = new bool[2];
+        enabledRenderers[0] = Random.Range(0, 2) == 0;//Axe
+        enabledRenderers[1] = Random.Range(0, 2) == 0; ;//Shield
+
+        object[] objs = new object[1]; // Put our bool data in an object array, to send
+        objs[0] = enabledRenderers;*/
+
+        // Spawn our local player
+        //PhotonNetwork.Instantiate(this.playerPrefabName, transform.position, Quaternion.identity, 0/*, objs*/);
+        //spawnManager.spawnStart();
+    }
+
+    /* void OnGUI()
+     {
+         if (PhotonNetwork.room == null) return; //Only display this GUI when inside a room
+
+         if (GUILayout.Button("Leave Room"))
+         {
+             PhotonNetwork.LeaveRoom();
+         }
+     }*/
+
+    void OnDisconnectedFromPhoton()
+    {
+        Debug.LogWarning("OnDisconnectedFromPhoton");
+    }
+}
---
+++ WonderJam/Assets/Scripts/CRandomMatchmaking.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CRandomMatchmaking : MonoBehaviour
+{
+    GUIStyle style;
+    bool Joined = false;
+    bool Created = false;
+    bool RandomJoined = false;
+    bool SelectJoined = false;
+    bool ChampSelect = false;
+    void Awake()
+    {
+        //PhotonNetwork.logLevel = NetworkLogLevel.Full;²
+
+        //Connect to the main photon server. This is the only IP and port we ever need to set(!)
+        if (!PhotonNetwork.connected)
+            PhotonNetwork.ConnectUsingSettings("v3.0"); // version of the game/demo. used to separate older clients from newer ones (e.g. if incompatible)
+
+        //Load name from PlayerPrefs
+        PhotonNetwork.playerName = PlayerPrefs.GetString("playerName", "Pascal" + Random.Range(1, 9999));
+
+        //Set camera clipping for nicer "main menu" background
+        Camera.main.farClipPlane = Camera.main.nearClipPlane + 0.1f;
+
+
+
+    }
+
+    private string roomName = "myRoom";
+    private Vector2 scrollPos = Vector2.zero;
+
+    void OnGUI()
+    {
+
+        if (!PhotonNetwork.connected)
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), GUIContent.none);
+            ShowConnectingGUI();
+            return;   //Wait for a connection
+        }
+
+        if (PhotonNetwork.room != null)
+            return; //Only when we're not in a Room       
+
+        GUI.Box(new Rect(0, 0, Screen.width, Screen.height), GUIContent.none);
+
+
+        GUILayout.BeginArea(new Rect(0, 0, 80, 300));
+        ShowPing();
+
+        GUILayout.EndArea();
+
+
+
+        GUILayout.BeginArea(new Rect((Screen.width - 400) / 2, (Screen.height - 300) / 2, 400, 300));
+
+        GUILayout.Label("Main Menu");
+
+        //Player name
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Player name:", GUILayout.Width(150));
+        PhotonNetwork.playerName = GUILayout.TextField(PhotonNetwork.playerName);
+        if (GUI.changed)//Save name
+            PlayerPrefs.SetString("playerName", PhotonNetwork.playerName);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(15);
+
+
+        //Join room by title
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("JOIN ROOM:", GUILayout.Width(150));
+        roomName = GUILayout.TextField(roomName);
+        if (GUILayout.Button("GO"))
+        {
+            Joined = true;
+            ChampSelect = true;
+            PhotonNetwork.JoinRoom(roomName);
+        }
+        GUILayout.EndHorizontal();
+
+        //Create a room (fails if exist!)
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("CREATE ROOM:", GUILayout.Width(150));
+        roomName = GUILayout.TextField(roomName);
+        if (GUILayout.Button("GO"))
+        {
+            // using null as TypedLobby parameter will also use the default lobby
+            Joined = true;
+            ChampSelect = true;
+            PhotonNetwork.CreateRoom(roomName, new RoomOptions() { MaxPlayers = 10 }, TypedLobby.Default);
+        }
+        GUILayout.EndHorizontal();
+
+        //Join random room
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("JOIN RANDOM ROOM:", GUILayout.Width(150));
+        if (PhotonNetwork.GetRoomList().Length == 0)
+        {
+            GUILayout.Label("..no games available...");
+
+        }
+        else
+        {
+            if (GUILayout.Button("GO"))
+            {
+                RandomJoined = true;
+                ChampSelect = true;
+                PhotonNetwork.JoinRandomRoom();
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(30);
+        GUILayout.Label("ROOM LISTING:");
+        if (PhotonNetwork.GetRoomList().Length == 0)
+        {
+            GUILayout.Label("..no games available..");
+        }
+        else
+        {
+            //Room listing: simply call GetRoomList: no need to fetch/poll whatever!
+            scrollPos = GUILayout.BeginScrollView(scrollPos);
+            foreach (RoomInfo game in PhotonNetwork.GetRoomList())
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(game.name + " " + game.playerCount + "/" + game.maxPlayers);
+                if (GUILayout.Button("JOIN"))
+                {
+                    SelectJoined = true;
+                    ChampSelect = true;
+                    PhotonNetwork.JoinRoom(game.name);
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+        }
+
+        GUILayout.EndArea();
+    }
+
+    void ShowPing()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Ping :");
+        int ping = PhotonNetwork.GetPing();
+        if (ping <= 50)
+            GUI.contentColor = Color.green;
+        else if (ping <= 100)
+            GUI.contentColor = Color.yellow;
+        else
+            GUI.contentColor = Color.red;
+        GUILayout.Label(ping.ToString());
+        GUI.contentColor = Color.white;
+        GUILayout.EndHorizontal();
+    }
+    void ShowConnectingGUI()
+    {
+        GUILayout.BeginArea(new Rect((Screen.width - 400) / 2, (Screen.height - 300) / 2, 400, 300));
+
+        GUILayout.Label("Connecting to Photon server.");
+        GUILayout.Label("Hint: This demo uses a settings file and logs the server address to the console.");
+
+        GUILayout.EndArea();
+    }
+
+    public void OnConnectedToMaster()
+    {
+        // this method gets called by PUN, if "Auto Join Lobby" is off.
+        // this demo needs to join the lobby, to show available rooms!
+
+        PhotonNetwork.JoinLobby();  // this joins the "default" lobby
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel(PhotonNetwork.room.CustomProperties["propertyName"].ToString());
+    }
+    
+}
+
