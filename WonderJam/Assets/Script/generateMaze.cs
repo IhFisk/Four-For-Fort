059857d0e63dfa852206b4d48ea4f13800da05a9
Hugo <ihfisk42@gmail.com>
2019-03-02 11:54:17 -0500

    iredfgf

--- WonderJam/Assets/Script/generateMaze.cs
+++ WonderJam/Assets/Script/generateMaze.cs
@@ -10,7 +10,6 @@ public class generateMaze : MonoBehaviour
     public Vector3 direction;
     
     private Vector3 baseTransform = new Vector3(0, 0, 0);
-    private bool isGenerate = true;
 
     // Start is called before the first frame update
     void Start()
