27a39dbafae0248fc679524338b2137b9ff2828b
Samuel Busson <samuel.busson@hotmail.fr>
2019-03-02 09:46:00 -0500

    Copy scene

--- WonderJam/Assets/Script/Misc/TeleportPlayer.cs
+++ WonderJam/Assets/Script/Misc/TeleportPlayer.cs
@@ -6,7 +6,14 @@ public class TeleportPlayer : MonoBehaviour
 {
 
     Activate active;
-    
+
+    public GameObject particleEffect;
+
+    private Transform newPos;
+    private bool pos_found = false;
+    private GameObject player;
+
+    private float time = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +31,20 @@ public class TeleportPlayer : MonoBehaviour
             {
                 if (active.getPlayer() && go.GetPhotonView() == active.getPlayer().GetPhotonView())
                 {
-                    active.getPlayer().transform.position = transform.position;
+                    player = active.getPlayer();
+                    pos_found = true;
+                    active.setActive(false);
                 }
+            }
+        }
+
+        if (pos_found)
+        {
+            time += Time.deltaTime;
+            //Instantiate();
+            if(time > 0.5f)
+            {
+
             }
         }
     }
