6a7371b9b74f6ecaf054a6cfd55a1be879253770
Samuel Busson <samuel.busson@hotmail.fr>
2019-03-01 19:53:50 -0500

    Add crate generator

---
+++ WonderJam/Assets/Script/cloud.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour
+{
+
+    public float speedValue = 0.31f;
+    private MeshFilter mesh;
+
+    private Vector3 spawnPosition = new Vector3(0, 0, 0);
+
+
+    void Start()
+    {
+        mesh = GetComponentInChildren<MeshFilter>();
+        spawnPosition = mesh.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speedValue * transform.forward * Time.deltaTime;
+    }    
+
+    public void resetPosition()
+    {
+        transform.position = spawnPosition;
+    }
+
+    public void OnBecameInvisible()
+    {
+        resetPosition();
+    }
+}
---
+++ WonderJam/Assets/Script/generator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour
+{
+
+    public GameObject goobjectToInstance;
+    public int numberObjectToSpawnPerLine;
+    public int numberOfLine;
+
+
+    public Vector3 direction;
+
+    private Vector3 baseTransformValue = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseTransformValue = transform.position;
+
+        for (int j = 0; j < numberOfLine; j++)
+        {
+            transform.position = new Vector3(baseTransformValue.x, transform.position.y, transform.position.z);
+                
+            for (int i = 0; i < numberObjectToSpawnPerLine; i++)
+            {
+                Instantiate(goobjectToInstance, transform.position, Quaternion.identity);
+                transform.position += new Vector3(direction.x, 0, 0);
+            }
+            transform.position += new Vector3(0, 0, direction.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
