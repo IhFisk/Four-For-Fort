89afbe6151ff5b24db5416e3dc4acf627ce1e91a
FlDeLima <florian.de_lima@hotmail.com>
2019-03-02 12:37:41 -0500

    2 players

--- WonderJam/Assets/Scripts/Network/CGameManager.cs
+++ WonderJam/Assets/Scripts/Network/CGameManager.cs
@@ -8,6 +8,7 @@ public class CGameManager : Photon.MonoBehaviour
     // this is a object name (must be in any Resources folder) of the prefab to spawn as player avatar.
     // read the documentation for info how to spawn dynamically loaded game objects at runtime (not using Resources folders)
     public string playerPrefabName = "Personnage";
+    public string player2PrefabName = "Personnage";
     public GameObject rouge;
     public GameObject bleu;
     private string roomName = "myRoom2";
@@ -70,29 +71,47 @@ public class CGameManager : Photon.MonoBehaviour
             PhotonNetwork.player.SetTeam(PunTeams.Team.blue);
             cube = bleu;
         }
-            
 
-        
-        GameObject player = PhotonNetwork.Instantiate(this.playerPrefabName, cube.transform.position, Quaternion.identity, 0/*, objs*/);
-        
-            
+        string name = this.playerPrefabName;
+        if (PhotonNetwork.playerList.Length %2==1)
+            name = this.player2PrefabName;
+        else
+            name = this.playerPrefabName;
+        GameObject player = PhotonNetwork.Instantiate(name, cube.transform.position, Quaternion.identity, 0/*, objs*/);
+
         //spawnManager.spawnStart();
     }
 
-    /* void OnGUI()
+     void OnGUI()
      {
-         if (PhotonNetwork.room == null) return; //Only display this GUI when inside a room
+        if (PhotonNetwork.room == null) return; //Only display this GUI when inside a room
 
-         if (GUILayout.Button("Leave Room"))
-         {
-             PhotonNetwork.LeaveRoom();
-         }
-     }*/
+        /*if (GUILayout.Button("Leave Room"))
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+        */
+        ShowPing();
+     }
 
     void OnDisconnectedFromPhoton()
     {
         Debug.LogWarning("OnDisconnectedFromPhoton");
     }
 
-
+    void ShowPing()
+    {
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Ping :");
+        int ping = PhotonNetwork.GetPing();
+        if (ping <= 50)
+            GUI.contentColor = Color.green;
+        else if (ping <= 100)
+            GUI.contentColor = Color.yellow;
+        else
+            GUI.contentColor = Color.red;
+        GUILayout.Label(ping.ToString());
+        GUI.contentColor = Color.white;
+        GUILayout.EndHorizontal();
+    }
 }
