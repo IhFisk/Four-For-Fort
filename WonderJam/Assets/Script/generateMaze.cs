8660f7faf20519417bb3e481b57ec1d455d50374
Hugo <ihfisk42@gmail.com>
2019-03-02 11:32:41 -0500

    change things

--- WonderJam/Assets/Script/generateMaze.cs
+++ WonderJam/Assets/Script/generateMaze.cs
@@ -8,41 +8,30 @@ public class generateMaze : MonoBehaviour
     public int lines;
     public int columns;
     public Vector3 direction;
-
-    private Activate active;
+    
     private Vector3 baseTransform = new Vector3(0, 0, 0);
-
-    private bool once = false;
     private bool isGenerate = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        active = GetComponent<Activate>();
         baseTransform = transform.position;
+
+        for (int i = 0; i < lines; i++)
+        {
+            transform.position = new Vector3(baseTransform.x, baseTransform.y, baseTransform.z);
+            for (int j = 0; j < columns; j++)
+            {
+                Instantiate(objectToInstance, transform.position, Quaternion.identity);
+                transform.position += new Vector3(direction.x, 0, 0);
+            }
+            transform.position += new Vector3(0, 0, direction.z);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(active.getActive() && !once)
-        {
-            for(int i = 0; i < lines; i++)
-            {
-                transform.position = new Vector3(baseTransform.x, baseTransform.y, baseTransform.z);
-                for(int j = 0; j < columns; j++)
-                {
-                    Instantiate(objectToInstance, transform.position, Quaternion.identity);
-                    transform.position += new Vector3(direction.x, 0, 0);
-                }
-                transform.position += new Vector3(0, 0, direction.z);
-            }
-            once = true;
-        }
-    }
 
-    public void Activate()
-    {
-        isGenerate = false;
     }
 }
