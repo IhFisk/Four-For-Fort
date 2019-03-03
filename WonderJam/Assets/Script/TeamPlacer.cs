b1aceb5e726a15976b2c0760c27d856df5c2ea63
Samuel Busson <samuel.busson@hotmail.fr>
2019-03-03 09:54:23 -0500

    Add ui

---
+++ WonderJam/Assets/Script/TeamPlacer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamPlacer : MonoBehaviour
+{
+    private Vector3 startPositon;
+    private Vector3 endPositon;
+
+    private float distanceBetweenPoints;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startPositon = GameObject.FindGameObjectWithTag("StartPosition").transform.position;
+        endPositon = GameObject.FindGameObjectWithTag("EndPosition").transform.position;
+
+        distanceBetweenPoints = Vector3.Distance(startPositon, endPositon);
+    }
+
+    public float getDistanceBetweenPoint()
+    {
+        return distanceBetweenPoints;
+    }
+
+    public Vector3 getEndPositon()
+    {
+        return endPositon;
+    }
+
+
+}
---
+++ WonderJam/Assets/SliderUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliderUI : MonoBehaviour
+{
+
+    public Transform positionEquipe;
+
+    private Vector3 startPositonUI = new Vector3(0, 0, 0);
+    public Transform endPositonUI;
+
+    private float distanceTotal;
+
+    private Vector3 endPositionMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TeamPlacer tp = GetComponent<TeamPlacer>();
+        distanceTotal = tp.getDistanceBetweenPoint();
+        endPositionMap = tp.getEndPositon();
+        startPositonUI = transform.position;
+
+        Debug.Log(endPositionMap);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(positionEquipe.position, endPositionMap);
+
+        transform.position = Vector3.Lerp(startPositonUI, endPositonUI.position, distance/ distanceTotal);
+    }
+}
