f2baf390d14e36c995705e32c0d9a915291cae42
FlDeLima <florian.de_lima@hotmail.com>
2019-03-02 20:23:06 -0500

    add timer bitch

---
+++ WonderJam/Assets/Scripts/Network/CAfficherTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+public class CAfficherTimer : Photon.PunBehaviour
+{
+
+
+    public Text TxtTimer;
+    public CTimer timer;
+    public string nomTimer;
+
+    void Start()
+    {        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        TxtTimer.text = timer.StringTimer;
+    }
+}
---
+++ WonderJam/Assets/Scripts/Network/CTimer.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+
+public class CTimer : Photon.PunBehaviour
+{
+
+
+    public float timer = 0;
+    public float GameLenght = 300;
+
+    [HideInInspector] public string StringTimer;
+    private bool gameEnded = false;
+    private float time0 = 0;
+    private bool Started = false;
+    private bool Quit = false;
+    public float countdown = 0;
+    public float countdownValue = 10;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        SerializeState(stream, info);
+    }
+
+    void SerializeState(PhotonStream stream, PhotonMessageInfo info)
+    {
+
+    }
+
+    void Update()
+    {
+        if (PhotonNetwork.inRoom)
+        {
+            if (!Started)
+            {
+                if (countdown > countdownValue)
+                    StartTimer();
+                if(PunTeams.PlayersPerTeam[PunTeams.Team.blue].Count ==2 && PunTeams.PlayersPerTeam[PunTeams.Team.red].Count == 2)
+                {
+                    countdown += Time.deltaTime;
+                    TimeSpan ts = TimeSpan.FromSeconds(countdownValue - countdown);
+                    StringTimer = new DateTime(ts.Ticks).ToString("mm:ss");
+                }
+
+            }
+            if (Started)
+            {
+                timer = (float)PhotonNetwork.time - time0;
+                float t = timer - time0;
+                TimeSpan ts = TimeSpan.FromSeconds(timer);
+                if (ts.Seconds >= 0)
+                    StringTimer = new DateTime(ts.Ticks).ToString("mm:ss");
+                if (timer > GameLenght)
+                {
+                    if (!gameEnded)
+                    {
+                        gameEnded = true;
+                        Time.timeScale = 0; // arrete le temps du jeu
+                    }
+                    else if (Quit)
+                    {
+                        Debug.Log("youou");
+                        endGame();
+                    }
+                }
+            }
+        }
+    }
+
+    void endGame()
+    {
+        /*CJoueur[] players = FindObjectsOfType<CJoueur>(); // recupere tous les joueurs
+        foreach (CJoueur player in players)
+        {
+            player.endGame();
+            Debug.Log("end");
+            Time.timeScale = 1;
+            PhotonNetwork.LeaveRoom();
+        }
+        CCoolDown[] cds = FindObjectsOfType<CCoolDown>();
+        foreach (CCoolDown cd in cds)
+        {
+            cd.disableComp();
+        }*/
+    }
+
+ 
+    public void StartTimer()
+    {
+        //Time.timeScale = 1;
+        Started = true;
+        if (PhotonNetwork.isMasterClient)
+        {
+            float referenceTime = (float)PhotonNetwork.time;
+            Hashtable setReferenceTime = new Hashtable() { { "RefTime", referenceTime } };
+            PhotonNetwork.room.SetCustomProperties(setReferenceTime);
+        }
+        Hashtable h = PhotonNetwork.room.CustomProperties;
+        time0 = (float)h["RefTime"];
+    }
+
+    IEnumerator OnLeftRoom()
+    {
+        while (PhotonNetwork.room != null || PhotonNetwork.connected == false)
+            yield return 0;
+    }
+}
+
