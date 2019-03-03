d38d2b60ab8c384f064665a9c3ef6ce316ed704e
FlDeLima <florian.de_lima@hotmail.com>
2019-03-03 11:55:05 -0500

    add end screen

---
+++ WonderJam/Assets/Scripts/TriggerFin.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFin : MonoBehaviour
+{
+    
+    public bool blue;
+    public bool red;
+    public bool once=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!once)
+        {
+            PhotonView pv = other.gameObject.GetPhotonView();
+            if (pv.owner.GetTeam() == PunTeams.Team.blue)
+            {
+                blue = true;
+            }
+            if (pv.owner.GetTeam() == PunTeams.Team.red)
+            {
+                red = true;
+            }
+        }
+        
+    }
+}
