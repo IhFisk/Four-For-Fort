d6e812bd6d869fee0d65e86211c71f5b6a531848
Hugo <ihfisk42@gmail.com>
2019-03-03 11:29:10 -0500

    add feedback on play button

---
+++ WonderJam/Assets/GUI/playOver.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playOver : MonoBehaviour
+{
+    public Sprite over;
+    private Image imagePlay;
+    private Sprite notOver;
+    public bool isOvered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imagePlay = GetComponent<Image>();
+        notOver = imagePlay.sprite;
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        isOvered = true;
+        imagePlay.sprite = over;
+    }
+
+    private void OnMouseExit()
+    {
+        isOvered = false;
+        imagePlay.sprite = notOver;
+    }
+}
--- WonderJam/Assets/Script/SoundOver.cs
+++ WonderJam/Assets/Script/SoundOver.cs
@@ -1,28 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundOver : MonoBehaviour
 {
     public AudioClip audioClip;
+    public Sprite playOver;
 
     private AudioSource audioSource;
+    private Canvas playCanvas;
+    private Image playImage;
+    private Sprite notOver;
 
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         audioSource.clip = audioClip;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        playCanvas = GetComponentInChildren<Canvas>();
+        playImage = playCanvas.GetComponentInChildren<Image>();
+        notOver = playImage.sprite;
     }
 
     private void OnMouseEnter()
     {
         audioSource.Play();
+        playImage.sprite = playOver;
+    }
+
+    private void OnMouseExit()
+    {
+        playImage.sprite = notOver;
     }
 }
